0d865e9ec6c95884a251a3b0cc1fb2130a051f67
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-05 23:53:18 +0100

    first commit

---
+++ lodash-examples.js
@@ -0,0 +1,20 @@
+// Load the full build.
+var _ = require('lodash');
+// Load the core build.
+var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
+function something() {
+	let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	return _.filter(numberArray, n => n % 2 == 0);
+}
