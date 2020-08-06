4c669e6a578331821d6f7095ce2823e213d091de
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-06 21:21:10 +0100

    Add reject.js

---
+++ collection/reject.js
@@ -0,0 +1,9 @@
+var _ = require('../lodash');
+
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function getOddNumbers() {
+	return _.reject(numberArray, n => n % 2 === 0);
+}
+
+console.log(getOddNumbers());
