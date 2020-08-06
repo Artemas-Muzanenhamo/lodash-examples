4a0b7478570b0ed0a2c3d9940b86ee455de869fb
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-06 17:29:46 +0100

    Add concat example

--- README.md
+++ README.md
@@ -19,3 +19,33 @@ return _.reject(numberArray, n => n % 2 == 0);
 // output
 [ 1, 3, 5, 7, 9 ]
 ```
+
+## Concat
+- Joining an object to other objects
+
+```javascript
+let basketBallPlayers = [
+	{ name: 'Kobe',	surname: 'Bryant', position: 'SG', active: false},
+	{ name: 'LeBron', surname: 'James', position: 'SF', active: true},
+	{ name: 'Michael', surname: 'Jordan', position: 'SG', active: false},
+	{ name: 'Dwayne', surname: 'Wade', position: 'SG', active: false},
+	{ name: 'Kevin', surname: 'Garnett', position: 'PF', active: false}
+];
+
+
+const newPlayer = { name: 'Artemas', surname: 'Prime', position: 'PG', active: true};
+
+return _.concat(basketBallPlayers, newPlayer);
+
+
+// output 
+
+[
+	{name: 'Kobe', surname: 'Bryant', position: 'SG', active: false},
+	{name: 'LeBron', surname: 'James', position: 'SF', active: true},
+	{name: 'Michael', surname: 'Jordan', position: 'SG', active: false},
+	{name: 'Dwayne', surname: 'Wade', position: 'SG', active: false},
+	{name: 'Kevin', surname: 'Garnett', position: 'PF', active: false},
+	{name: 'Artemas', position: 'PG', active: true}
+];
+```
--- lodash-examples.js
+++ lodash-examples.js
@@ -4,13 +4,69 @@ var _ = require('./lodash');
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function getEvenNumbers() {
-	return _.filter(numberArray, n => n % 2 == 0);
+	return _.filter(numberArray, n => n % 2 === 0);
 }
 
 console.log(getEvenNumbers());
 
 function getOddNumbers() {
-	return _.reject(numberArray, n => n % 2 == 0);
+	return _.reject(numberArray, n => n % 2 === 0);
 }
 
 console.log(getOddNumbers());
+
+let basketBallPlayers = [
+	{
+		name: 'Kobe',
+		surname: 'Bryant',
+		position: 'SG',
+		active: false
+	},
+	{
+		name: 'LeBron',
+		surname: 'James',
+		position: 'SF',
+		active: true
+	},
+	{
+		name: 'Michael',
+		surname: 'Jordan',
+		position: 'SG',
+		active: false
+	},
+	{
+		name: 'Dwayne',
+		surname: 'Wade',
+		position: 'SG',
+		active: false
+	},
+	{
+		name: 'Kevin',
+		surname: 'Garnett',
+		position: 'PF',
+		active: false
+	}
+];
+
+function addPlayer() {
+	const newPlayer = {
+		name: 'Artemas',
+		surname: 'Prime',
+		position: 'PG',
+		active: true
+	};
+
+	return _.concat(basketBallPlayers, newPlayer);
+}
+
+console.log(addPlayer());
+
+
+[
+	{name: 'Kobe', surname: 'Bryant', position: 'SG', active: false},
+	{name: 'LeBron', surname: 'James', position: 'SF', active: true},
+	{name: 'Michael', surname: 'Jordan', position: 'SG', active: false},
+	{name: 'Dwayne', surname: 'Wade', position: 'SG', active: false},
+	{name: 'Kevin', surname: 'Garnett', position: 'PF', active: false},
+	{name: 'Artemas', position: 'PG', active: true}
+]
